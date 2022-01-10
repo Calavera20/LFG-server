cb6ffeaadd83cf3b318c6e7a56ccd9b508cd0252
Calavera20 <33523344+Calavera20@users.noreply.github.com>
2022-01-10 21:21:22 +0100

    try 10.2

--- build/typeDefs/GameCard.js
+++
@@ -1,7 +0,0 @@
-export const typeDefs = `
-    type GameCard {
-        id: ID
-        imageB64: String
-        title: String
-    }
-`;
--- build/typeDefs/Subscription.js
+++
@@ -1,8 +0,0 @@
-
-export const typeDefs = `
-
-    type Subscription {
-        messageAdded(channelId: String): Message
-        requestAdded(groupId: String): DecisionChange
-        groupPermissionChanged(groupId: String): PermissionChange
-    }`;
