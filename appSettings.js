4a837cbceeff5909d582ff3329858127ba321f92
Calavera20 <33523344+Calavera20@users.noreply.github.com>
2022-01-29 20:28:21 +0100

    appSettings jebait

---
+++ appSettings.js
@@ -0,0 +1,8 @@
+//Plik konfiguracyjny - wartości zmiennych process.env. 
+//znajdują się jedynie w konfiguracji platformy hostingowej Heroku
+
+//Sekret JWT służący do weryfikacji i podpisywania tokenów JWT
+JWTSecret = process.env.JWT_SECRET;
+
+//token połączenia z bazą MongoDB
+MongooseUri = process.env.JWT_SECRET;
