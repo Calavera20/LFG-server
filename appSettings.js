bc3be21da4c2d69a6800474d0eae8968c55f5306
Calavera20 <33523344+Calavera20@users.noreply.github.com>
2022-01-30 19:54:00 +0100

    add settings file

--- appSettings.js
+++ appSettings.js
@@ -1,8 +1,26 @@
-//Plik konfiguracyjny - wartości zmiennych process.env. 
+//Plik konfiguracyjny - opis wartości zmiennych process.env. 
 //znajdują się jedynie w konfiguracji platformy hostingowej Heroku
 
 //Sekret JWT służący do weryfikacji i podpisywania tokenów JWT
-JWTSecret = process.env.JWT_SECRET;
+process.env.JWT_SECRET;
 
 //token połączenia z bazą MongoDB
-MongooseUri = process.env.MongooseURI;
+process.env.MongooseURI;
+
+//PORT na którym ma zostać utworzony serwer
+process.env.PORT
+
+//nazwa konta gmail
+process.env.MAIL_USERNAME
+
+//hasło do konta gmail
+process.env.MAIL_PASSWORD
+
+//CLIENTID OAuth - konfiguracja API Gmail
+process.env.OAUTH_CLIENTID
+
+//CLIENT_SECRET OAuth - konfiguracja API Gmail
+process.env.OAUTH_CLIENT_SECRET
+
+//CLIENT_REFRESH_TOKEN OAuth - konfiguracja API Gmail
+process.env.OAUTH_REFRESH_TOKEN
