3ead89e4ccb3f5a6288c4f5bab64728fe9c6a953
Calavera20 <33523344+Calavera20@users.noreply.github.com>
2022-02-10 01:23:43 +0100

    fix problem with gmail api refresh token

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -171,12 +171,12 @@ export const resolvers = {
 		let transporter = nodemailer.createTransport({
 			service: 'gmail',
 			auth: {
-				type: 'OAuth2',
+				// type: 'OAuth2',
 				user: process.env.MAIL_USERNAME,
 				pass: process.env.MAIL_PASSWORD,
-				clientId: process.env.OAUTH_CLIENTID,
-				clientSecret: process.env.OAUTH_CLIENT_SECRET,
-				refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+				// clientId: process.env.OAUTH_CLIENTID,
+				// clientSecret: process.env.OAUTH_CLIENT_SECRET,
+				// refreshToken: process.env.OAUTH_REFRESH_TOKEN,
 			},
 		});
 
